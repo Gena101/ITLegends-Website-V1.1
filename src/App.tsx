1599ccbe6755c855107d24f8da2be48cccc239b9
ITBolt12 <bolt12@itlegends.co.za>
2025-11-18 11:05:32 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -581,7 +581,7 @@ function Footer() {
             <ul className="space-y-2 text-itsilver">
               <li><a href="/" className="hover:text-itred transition-colors">Home</a></li>
               <li><a href="/#about" className="hover:text-itred transition-colors">Who We Are</a></li>
-              <li><a href="#why-us" className="hover:text-itred transition-colors">Why Us</a></li>
+              <li><a href="/#services" className="hover:text-itred transition-colors">Our Services</a></li>
               <li><a href="#blog" className="hover:text-itred transition-colors">Blog</a></li>
             </ul>
           </div>
