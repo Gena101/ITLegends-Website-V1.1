bdaf0ee7fdba99d75865850f69dffcf573684d73
ITBolt12 <bolt12@itlegends.co.za>
2025-11-19 09:10:21 +0200

    Updated CloudBackup.tsx

--- src/pages/CloudBackup.tsx
+++ src/pages/CloudBackup.tsx
@@ -138,6 +138,7 @@ function ContentSection() {
           </div>
         </div>
       </section>
+      <SectionDivider />
 
       <section className="py-20 px-4 sm:px-6 lg:px-8 bg-itgray/20">
         <div className="max-w-7xl mx-auto">
