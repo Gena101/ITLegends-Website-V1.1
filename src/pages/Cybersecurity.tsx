916a436b6924385bbf288a8a239c603965c6dac7
ITBolt12 <bolt12@itlegends.co.za>
2025-11-19 09:08:18 +0200

    Updated Cybersecurity.tsx

--- src/pages/Cybersecurity.tsx
+++ src/pages/Cybersecurity.tsx
@@ -164,6 +164,7 @@ function ContentSection() {
           </div>
         </div>
       </section>
+      <SectionDivider />
 
       <section className="py-20 px-4 sm:px-6 lg:px-8 bg-itdark">
         <div className="max-w-7xl mx-auto">
@@ -203,6 +204,7 @@ function ContentSection() {
           </div>
         </div>
       </section>
+      <SectionDivider />
 
       <section className="py-20 px-4 sm:px-6 lg:px-8 bg-itgray">
         <div className="max-w-7xl mx-auto">
